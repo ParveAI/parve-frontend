c19682f86b79f9aa5d23e7daa42305122c6cf07a
nurullahkilic <75576033+nurullahkilic@users.noreply.github.com>
2023-04-17 23:22:14 +0300

    Styles updated.

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,6 @@
-import { Container } from "./index.styles";
+import styled from "styled-components";
 
 import Hero from "@/components/Hero";
-
 
 export default function Home() {
   return (
@@ -10,3 +9,9 @@ export default function Home() {
     </Container>
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
