030d35a033fa757fd57861c88e94f7e6fcfe7a6a
nurullahkilic <75576033+nurullahkilic@users.noreply.github.com>
2023-08-16 12:49:15 +0300

    changed modal structure

--- src/modals/components/UserInfoModal/index.js
+++ src/modals/components/UserInfoModal/index.js
@@ -6,7 +6,7 @@ import { toast } from "react-hot-toast";
 export default function UserInfoModal({ user, logout, closeModal, ...props }) {
   const router = useRouter();
   return (
-    <div className="relative w-[450px] rounded-xl bg-[#282828] px-10 py-8 max-md:w-[88vw] max-md:py-9 max-md:px-7">
+    <div className="glow relative w-[450px] rounded-xl bg-[#282828] px-10 py-8 max-md:w-[88vw] max-md:py-9 max-md:px-7">
       <CloseButton onClick={closeModal} />
       <div className="w-full flex flex-col items-center justify-start gap-5">
         <button
